0f57f8f4156796de23fb7f2f740bccc3b4f34072
li <1016371613@qq.com>
2016-10-09 14:11:33 +0800

    My Loader

--- Core/Loader.php
+++ Core/Loader.php
@@ -2,7 +2,10 @@
 namespace Core;
 class Loader
 {
-    static function autoload(){
-        
+    static function autoload($class){
+        $path=APP.".".$class.'.php';
+        $path=str_replace('\\','/',$path);
+        include ($path);
+        echo $path;
     }
 }
