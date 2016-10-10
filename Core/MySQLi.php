afb0c3fa7f2c1da1d0606eecb451124226df05dc
li <1016371613@qq.com>
2016-10-10 09:43:50 +0800

    My MySQLi

---
+++ Core/MySQLi.php
@@ -0,0 +1,43 @@
+<?php
+namespace Core;
+final class MySQLi
+{
+    private static $db = null;
+    private $conn;
+
+    //第一步 构造函数私有
+    private function __construct()
+    {
+        $this->conn = new \mysqli('127.0.0.1', 'root', 'root', 'train');
+
+        if ($this->conn->connect_errno)
+            die('连接数据库失败' . $this->conn->connect_error);
+        else
+            $this->conn->set_charset('utf8');
+    }
+
+    //第二步 外部调用的静态方法
+    static function getDb()
+    {
+        if (self::$db == null)
+            self::$db = new self();
+        return self::$db;
+    }
+
+    //第三步  防止外部克隆
+    private function __clone()
+    {
+
+    }
+
+    function query($sql)
+    {
+        return $this->conn->query($sql);
+    }
+
+    function __destruct()
+    {
+        $this->conn->close();
+    }
+}
+
