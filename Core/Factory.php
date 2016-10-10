96db5977558ac31a7f640ccc0fc46ef6f6a32b1a
li <1016371613@qq.com>
2016-10-10 15:30:15 +0800

    My Factory

--- Core/Factory.php
+++ Core/Factory.php
@@ -3,14 +3,20 @@
 
 namespace Core;
 
-
+use DataMap\Student;
 class Factory
 {
     static function createDb(){
         $db=MySQLi::getDb();
         Register::set('mysql',$db);
         return $db;
-        
-        
+    }
+    static function getStudent($id){
+        $student=Register::get($id);
+        if(!$student){
+            $student=new Student($id);
+            Register::set($id,$student);
+        }
+        return $student;
     }
 }
