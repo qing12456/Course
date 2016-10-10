9ec66eb5f992edc5afd5987ef44ab8aeab6812fd
li <1016371613@qq.com>
2016-10-10 10:18:13 +0800

    My index

--- index.php
+++ index.php
@@ -13,15 +13,42 @@ spl_autoload_register('\Core\Loader::autoload');
 
 //$db=Core\Single::getDb();
 
+//工厂模式
+$user=Factory\UserFactory::createUser('zhangsan',2);
+var_dump($user);
 
-//$user=Factory\UserFactory::createUser('zhangsan',2);
-//var_dump($user);
 
-$user=Factory\UserFactory::createUser('zhansan',2);
+//注册树
 $db=\Core\Factory::createDb();
 $db2=\Core\Register::get('mysql');
 $db1=\Core\Register::get('mysql');
-echo "<pre>";
-var_dump($db1);
-echo "</pre>";
+//echo "<pre>";
+//var_dump($db1);
+//echo "</pre>";
+
+
+
+class Cate{
+    protected $_strategy=null;
+    function index(){
+        $this->_strategy->showAd();
+        $this->_strategy->showCategory();
+    }
+    function setStrategy(\Core\IUserStrategy $strategy){
+        $this->_strategy=$strategy;
+    }
+}
+$c=new Cate;
+if($_GET['female']){
+    echo "female<br>";
+    $strategy=new \Core\FemaleStrategy();
+}
+elseif ($_GET['male']){
+    echo "Male<br>";
+    $strategy=new \Core\MaleStrategy();
+}
+$c->setStrategy($strategy);
+$c->index();
+
+
 
