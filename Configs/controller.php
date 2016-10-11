f8baf6ee3b3d0a98e034b04bc1302def65131d7e
li <1016371613@qq.com>
2016-10-11 13:28:59 +0800

    责任链模式

---
+++ Configs/controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HY
+ * Date: 2016/10/11
+ * Time: 11:16
+ */
+header("Content-type:text/html;charset=utf-8");
+$config=[
+    'home'=>[
+        'coffee'=>'Decorator\Coffee',
+    ]
+];
+return $config;
---
+++ Configs/db.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HY
+ * Date: 2016/10/11
+ * Time: 11:17
+ */
+header("Content-type:text/html;charset=utf-8");
+$config=[
+    'master'=>[
+        'type'=>'mysql',
+        'hostname'=>'localhost',
+        'username'=>'root',
+        'password'=>'root',
+        'dbName'=>'train',
+        'charset'=>'utf8',
+    ],
+    'slave'=>[
+        'slave1'=>[
+            'type'=>'mysql',
+            'hostname'=>'127.0.0.1',
+            'username'=>'root',
+            'password'=>'root',
+            'dbName'=>'train',
+            'charset'=>'utf8',
+        ],
+        'slave2'=>[
+            'type'      =>  'mysql',
+            'hostname'  =>  '127.0.0.1',
+            'username'  =>  'root',
+            'password'  =>  'root',
+            'dbName'    =>  'train',
+            'charset'   =>  'utf8',
+        ],
+    ],
+];
+return $config;
